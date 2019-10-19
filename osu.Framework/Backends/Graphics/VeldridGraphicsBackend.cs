ec13abeb8ad661f176d6d1384448071c37e6e087
Shane Woolcock <samahnub@gmail.com>
2019-10-19 13:07:41 +1030

    Create and use stub Veldrid backends

---
+++ osu.Framework/Backends/Graphics/VeldridGraphicsBackend.cs
@@ -0,0 +1,30 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using osu.Framework.Backends.Window;
+using osu.Framework.Configuration;
+using osu.Framework.Platform;
+using Veldrid;
+using Veldrid.Sdl2;
+using Veldrid.StartupUtilities;
+
+namespace osu.Framework.Backends.Graphics
+{
+    public class VeldridGraphicsBackend : GraphicsBackend
+    {
+        internal GraphicsDevice Device;
+
+        public override void Initialise(IGameHost host)
+        {
+            if (!(host.Window is VeldridWindowBackend window))
+                throw new Exception($"{nameof(VeldridGraphicsBackend)} requires a corresponding {nameof(VeldridWindowBackend)}");
+
+            Device = VeldridStartup.CreateGraphicsDevice(window.Implementation);
+        }
+
+        public override void Configure(ConfigManager<FrameworkSetting> config)
+        {
+        }
+    }
+}
---
+++ osu.Framework/Backends/Window/VeldridWindowBackend.cs
@@ -0,0 +1,95 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Collections.Generic;
+using System.Drawing;
+using osu.Framework.Bindables;
+using osu.Framework.Configuration;
+using osu.Framework.Platform;
+using Veldrid.Sdl2;
+using Veldrid.StartupUtilities;
+
+namespace osu.Framework.Backends.Window
+{
+    public class VeldridWindowBackend : WindowBackend
+    {
+        internal readonly Sdl2Window Implementation;
+
+        #region Read-only Bindables
+
+        private readonly BindableBool focused = new BindableBool();
+        public override IBindable<bool> Focused => focused;
+
+        private readonly BindableBool cursorInWindow = new BindableBool();
+        public override IBindable<bool> CursorInWindow => cursorInWindow;
+
+        private readonly BindableBool visible = new BindableBool();
+        public override IBindable<bool> Visible => visible;
+
+        #endregion
+
+        public override void Initialise(IGameHost host)
+        {
+        }
+
+        public override void Configure(ConfigManager<FrameworkSetting> config)
+        {
+        }
+
+        protected override IEnumerable<WindowMode> DefaultSupportedWindowModes => new[]
+        {
+            Configuration.WindowMode.Windowed,
+            Configuration.WindowMode.Borderless,
+            Configuration.WindowMode.Fullscreen,
+        };
+
+        public VeldridWindowBackend()
+        {
+            WindowCreateInfo windowCI = new WindowCreateInfo
+            {
+                X = 100,
+                Y = 100,
+                WindowWidth = 960,
+                WindowHeight = 540,
+                WindowTitle = "Test"
+            };
+            Implementation = VeldridStartup.CreateWindow(ref windowCI);
+        }
+
+        public override void Run()
+        {
+            while (Implementation.Exists)
+            {
+                Implementation.PumpEvents();
+            }
+        }
+
+        public override void Close()
+        {
+        }
+
+        public override Point PointToClient(Point point) => point;
+
+        public override Point PointToScreen(Point point) => point;
+
+        #region IDisposable
+
+        private bool isDisposed;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (!isDisposed)
+            {
+                if (disposing)
+                {
+                }
+
+                isDisposed = true;
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #endregion
+    }
+}
